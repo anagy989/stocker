a48eb01734c401aaa939acad6e3c031ce7ba66d1
Alexander Nagy <Alexander.Nagy@vincotech.com>
2020-11-28 12:54:53 +0100

    bicske

--- app/view/page/about.php
+++ app/view/page/about.php
@@ -1,4 +1,26 @@
 <div class="row">
-  <h1>Rólunk</h1>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25662.452628929244!2d18.928937736182938!3d47.461989989658896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741de295a121f6f%3A0xb25ac266a9135be8!2sStocker%20Kft.!5e0!3m2!1shu!2shu!4v1602942125744!5m2!1shu!2shu" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  <h1 class="m-2">Rólunk</h1>
+<div class="col">
+  <img src="" >
+</div>
+<div class="col">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25662.452628929244!2d18.928937736182938!3d47.461989989658896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741de295a121f6f%3A0xb25ac266a9135be8!2sStocker%20Kft.!5e0!3m2!1shu!2shu!4v1602942125744!5m2!1shu!2shu" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+</div>
+</div>
+<div class="row py-2">
+  <div class="col">
+    <h3>Üzletünk 1993 óta működik.</h3>
+  </div>
+</div>
+<div class="row">
+
+
+  <div class="col">
+    <h4>Cégünk az alábbiakkal foglalkozik:</h4>
+  </div>
+
+</div>
+<div class="row">
+  <p>Nagy mennyiségű vásárlás esetén,kiszállítást illetve kedvezményt tudunk biztosítani,halasztott fizetéssel.</p>
 </div>
--- app/view/page/home.php
+++ app/view/page/home.php
@@ -1,4 +1,8 @@
-<div class="row">
+<?php
+$baseUrl = "http://localhost:8080";
+?>
+<h2 class="py-5">Tekintse meg <a class="text-dark" href="<?php echo $baseUrl . '/index.php?page=kategoria&nev=Ablak-,%20és%20üvegtisztítók&pageno=1'; ?>"> termékeinket</a> online!</h2>
+<div class="row justify-content-center">
     <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -30,14 +34,34 @@
 
 
 
-<div class="row bg-secondary info-block">
-    <div class="col">
-
+<div class="row bg-secondary text-white info-block">
+    <div class="col col-md-auto justify-content-center ">
+        <ul style='list-style-type:none'>
+            <li>
+                <h3 class="my-2">Házhozszállítás</h3>
+            </li>
+            <li>
+                <h3 class="my-2">Kedvezmény</h3>
+            </li>
+            <li>
+                <h3 class="my-2">Halasztott fizetés</h3>
+            </li>
+        </ul>
     </div>
-    <div class="col">
-
+    <div class="col col-md-auto">
+        <div class="row justify-content-center">
+        <h4>Rendeléseiket leadhatják az alábbi elérhetőségeken:</h4>
+        <ul style='list-style-type:none'>
+            <li><i class="phone alternate icon"></i>Mobil: 06-20-44-88-715</li>
+            <li><i class="fax icon"></i>Fax: 06-23-428-314</li>
+            <li><i class="phone square alternate icon"></i>Vezetékes: 06-23-424-251</li>
+        </ul>
+        </div>
+      
     </div>
-    <div class="col">
-        <h3 class="m-4">Írjon nekünk</h3>
+    <div class="col col-md-auto justify-content-center">
+        <a class="text-white m-2" href="<?php echo $baseUrl . '/index.php?page=kapcsolat'; ?>">
+            <h2 class=" contact">Vagy írjon nekünk itt!</h2>
+        </a>
     </div>
 </div>
